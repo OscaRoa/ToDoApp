aaaa551e9c87462868973ae1379a72d95526bc14
Oscar Roa <oshardy@hotmail.es>
2015-10-15 12:44:02 -0700

    Keyboard control added in SecondViewController and outlets and actions added in both controller views. Interface test done.

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var toDoItemTextField: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.toDoItemTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 }
 
